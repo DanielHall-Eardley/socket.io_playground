cac164f859a9a4c76381f66e0b9a96dc847dc219
DanielHall-Eardley <350chevy8@gmail.com>
2020-04-27 18:12:18 -0400

    progress

--- public/script.js
+++ public/script.js
@@ -4,15 +4,9 @@ state = {
 
 window.onload = async () => {
   const response = await api('api', null, null, "GET")
-  console.log(response)
-  if (!response.data.namespaces?.length) {
-    state.error = 'No results'
-    setError()
-    return
-  }
-  
+  console.log(response.data.namespaces.length)
   const userList = document.querySelector('.user-list')
-  for (let user of response.data.namespaces.length) {
+  for (let user of response.data.namespaces) {
     const li = `
     <li class='list-result onclick='createRoom'>
       ${user.name}
@@ -25,17 +19,20 @@ window.onload = async () => {
 
 function setError () {
   const header = document.querySelector('header')
-  header.append(`<p>${state.error}</p>`)
+  const p = document.createElement('p')
+  p.innerText = state.error
+  header.append(p)
 } 
 
-async function api (url, body, header, method) {
+
+async function api (url, body, headers, method) {
   let res; 
 
   if (method === "GET") {
     res = await fetch("http://localhost:5000/" + url)
   } else {
     res = await fetch("http://localhost:5000/" + url, {
-      header: header,
+      headers: headers,
       body: body,
       method: method
     })
@@ -50,9 +47,30 @@ async function api (url, body, header, method) {
   return res.json()
 }
 
-function register (e) {
+async function register (e) {
   e.preventDefault()
-  console.log(e.target.name.value)
+
+  const body = JSON.stringify({
+    name: e.target.name.value
+  })
+
+  const headers = {
+    'Content-Type': 'application/json'
+  }
+  console.log(body)
+  const response = await api('api/signup', body, headers, 'POST')
+  console.log(response)
+
+  const userList = document.querySelector('.user-list')
+  for (let user of response.data.namespaces) {
+    const li = `
+    <li class='list-result onclick='createRoom'>
+      ${user.name}
+      <input type='hidden' value=${user.id}/>
+    </li>`
+
+    userList.append(li)
+  }
 }
 
 function message (e) {
--- server.js
+++ server.js
@@ -19,20 +19,18 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.static(__dirname + "/public"))
 
-app.post('api/signup', (req, res, next) => {
+app.post('/api/signup', (req, res, next) => {
   const namespace = new NameSpace(uuid(), req.body.name)
-  console.log(namespace)
   state.namespaces.push(namespace)
-  res.status(200).json({data: state.namespaces})
+  res.status(200).json({data: state})
 })
 
-app.get('api/send', (req, res, next) => {
+app.get('/api/send', (req, res, next) => {
   
 })
 
 app.use("/api", (req, res, next) => {
-  console.log("f")
-  res.status(200).json({data: state.namespaces})
+  res.status(200).json({data: state})
 })
 
 app.use((error, req, res, next) => {
